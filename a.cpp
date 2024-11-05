fe4b4e7d3675b15a6adb82f13d82692312ba5a40
2CHAEHOON6 <lch9826@gmail.com>
2024-11-05 23:00:53 +0900

    first commit in Windows

--- a.cpp
+++ a.cpp
@@ -1,10 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-int a = 1;
-auto b =1;
+int b =1;
 
 int main(){
     cout << b <<"\n"; // 1
-    cout << typeid(b).name() << "\n"; //i
-    return 0;
+	return 0;
 }
